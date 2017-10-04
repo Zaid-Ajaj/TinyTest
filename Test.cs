59d75578f172ad5aa87630a6d35d2e154030017b
Zaid-Ajaj <zaid.naom@gmail.com>
2017-10-04 14:53:21 +0200

    Add project files.

---
+++ Test.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TinyTest
+{
+    public class TestDefinition
+    {
+        public string Name { get; set; }
+        public bool Failed { get; set; }
+        public Exception Exception { get; set; } = null;
+    }
+
+
+    public static class Test
+    {
+        private static Action<string, Exception> onError = (name, ex) =>
+        {
+            Error($"Failed: {name}");
+            Console.WriteLine($"{ex.GetType().Name}: {ex.Message}");
+            Console.WriteLine(ex.StackTrace);
+        };
+
+
+         
+        private static void Error(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        private static void Success(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void Fail(string msg = "")
+        {
+            if (!string.IsNullOrEmpty(msg))
+            {
+                throw new Exception(msg);
+            }
+
+            throw new Exception();
+        }
+
+        public static void Equal<T>(T x, T y) where T : IEquatable<T>
+        {
+            if (!x.Equals(y)) 
+            {
+                Fail();
+            } 
+        }
+
+
+        public static void OnError(Action<string, Exception> handler)
+        {
+            if (handler != null)
+            {
+                onError = handler;
+            }
+        }
+
+        public static void Case(string name, Action handler)
+        {
+            try
+            {
+                handler();
+                Success($"Success: {name}");
+            }
+            catch(Exception ex)
+            {
+                onError(name, ex);
+            }
+        }
+
+        public static Task CaseAsync(string name, Func<Task> handler)
+        {
+            try
+            {
+                var task = handler();
+                task.Wait();
+                Success($"Success: {name}");
+                return task;
+            }
+            catch (Exception ex)
+            {
+                onError(name, ex);
+                return Task.Run(() => { });
+            }
+        }
+    }
+}
---
+++ TinyTest.Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using TinyTest;
+
+namespace TinyTest.UI
+{
+    class Program
+    {
+        static int Add(int x, int y) => x + y;
+
+        static void Main(string[] args)
+        {
+            Test.Case("Add(x, y) works", () =>
+            {
+                var result = Add(1, Add(2, Add(3, Add(4, 5))));
+                Test.Equal(result, 15);
+            });
+
+            Test.CaseAsync("Async tests too!", async () =>
+            {
+                await Task.Delay(1000);
+                Test.Equal(1, 1);
+            });
+        }
+    }
+}
